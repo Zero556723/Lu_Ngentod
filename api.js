0ff3ee55a4d05c7b5d5b972fd41eec93dc180e4a
@Revv404 <a8631720@gmail.com>
2025-09-22 14:54:49 +0700

    Delete api.js

--- api.js
+++
@@ -1,7 +0,0 @@
-async function sendToTelegram(text) {
-  await fetch('/api/send', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ text })
-  });
-}
